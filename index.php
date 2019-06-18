b4b4b465be586ca8a27643124162554317a2ea69
faomjok8643 <51964098+faomjok8643@users.noreply.github.com>
2019-06-18 11:47:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.amazon.com/Apple-iPhone-XR-64GB-exclusively/dp/B07K97BQDF");
 die();
 ?>
